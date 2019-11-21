9c9217c2f9f2e8109e601750a056c7b72e6f1c8c
nishantg95 <nishant.groveris@gmail.com>
2019-11-21 17:01:05 -0600

    Changed Spring container configuration from XML to Java
    -includes removing web.xml, spring-servlet.xml
    - AppConfig imitates spring-servlet.xml behavior

---
+++ src/main/java/com/nishant/configurations/AppConfig.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.nishant.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author nishant.b.grover
+ *
+ */
+@Configuration
+@EnableWebMvc
+@PropertySource(value = { "classpath:application.properties" })//needs to be in Hibernateconfig
+@ComponentScan(basePackages="com.nishant")
+public class AppConfig extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+}
---
+++ src/main/java/com/nishant/configurations/AppInitializer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.nishant.configurations;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author nishant.b.grover
+ *
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+
+}
