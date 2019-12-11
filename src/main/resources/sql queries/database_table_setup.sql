ade913c3cc09bf6210dd89941a7926638813e6b0
nishantg95 <nishant.groveris@gmail.com>
2019-12-11 17:46:58 -0600

    Adding schema for the repository items table

--- src/main/resources/sql queries/database_table_setup.sql
+++ src/main/resources/sql queries/database_table_setup.sql
@@ -19,3 +19,15 @@ ALTER TABLE `grocerymanagementapp`.`item`
 CHANGE COLUMN `date_today` `purchase_date` DATE NOT NULL ;
 
 
+---------------------------------------------------
+
+CREATE TABLE `grocerymanagementapp`.`repository_items` (
+  `rItemId` INT NOT NULL,
+  `rtemName` VARCHAR(45) NOT NULL,
+  `rDate_U_P` VARCHAR(45) NULL COMMENT 'Repository Item\'s UNOPENED PANTRY date',
+  `rDate_O_P` VARCHAR(45) NULL COMMENT 'Repository Item\'s OPENED PANTRY date',
+  `rDate_U_F` VARCHAR(45) NULL COMMENT 'Repository Item\'s UNOPENED Freezer date',
+  `rDate_O_F` VARCHAR(45) NULL COMMENT 'Repository Item\'s OPENED Freezer date',
+  `rDate_U_R` VARCHAR(45) NULL COMMENT 'Repository Item\'s UNOPENED REFRIGERATED date',
+  `rDate_O_R` VARCHAR(45) NULL COMMENT 'Repository Item\'s OPENED REFRIGERATED date',
+  PRIMARY KEY (`rItemId`));
